e9e78761dc2aca5eec242a010dc7e128066fd2d6
sharathKumarpk <sharathkumar6841@gmail.com>
2024-05-22 12:17:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [count,setCount]=useState(0)
+  const increment = ()=>{
+    if (count<10){
+      setCount(count+1)
+    }else {
+      setCount(count)
+    }
+ 
+  }
+  const reset = () =>{
+    setCount(0)
+  }
+  const decrement=()=>{
+    if (count>1){
+      setCount(count-1)
+    }else{
+      setCount(count)
+    }
+    
+  }
+
+  
+  return(
+  <div className="bg-black min-h-screen flex flex-col text-center justify-center items-center gap-10">
+    <span className="text-white text-7xl">{count}</span>
+    <div className="flex  gap-10  justify-center items-center">
+
+<button onClick={increment} className="bg-green-500 gap-16 px-4 py-2 rounded-md">increment</button>
+<button onClick={reset} className="bg-yellow-500 gap-16 px-4 py-2 rounded-md">reset</button>
+<button onClick={decrement} className="bg-orange-500 gap-16 px-4 py-2 rounded-md">decrement</button>
+</div>
+  </div>
+   
   );
 }
 
